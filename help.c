3cc00ab6e64209a8629e2bb902d9e5b6d347e20d
Federico Gentili <80174849+GeNTiLT@users.noreply.github.com>
2021-03-10 11:28:41 +0100

    Add files via upload

--- help.c
+++ help.c
@@ -8,19 +8,32 @@ int github(int a);
 int gmailprincipale(int a);
 int gmailscuola(int a);
 int gmailpanzette(int a);
+int vaiaeseguibili(int a);
+int vaiacodici(int a);
+int creaprogrammajava(int a);
+int compilajava(int a);
+int vaiajava(int a);
 int main(void)
 {
 	int scelta;
-	
-	printf("COMANDI ESEGUIBILI:\n");
-	printf("1. Crea un file di testo\n");
-	printf("2. Crea un file sorgente per un programma\n");
-	printf("3. Compila programma\n");
-	printf("4. vai su GitHub\n");
-	printf("5. Vai nella mail principale\n");
-	printf("6. Vai nella mail di scuola\n");
-	printf("7. Vai nella mail delle panzette\n\n");
-	printf("Che comando vuoi eseguire?: ");
+	printf("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n");
+	printf("*	COMANDI ESEGUIBILI:						    *\n");
+	printf("*									    *\n");
+	printf("*	01. Crea un file di testo					    *\n");
+	printf("*	02. Crea/Modifica un file sorgente per un programma C		    *\n");
+	printf("*	03. Compila programma C						    *\n");
+	printf("*	04. vai su GitHub						    *\n");
+	printf("*	05. Vai nella mail principale					    *\n");
+	printf("*	06. Vai nella mail di scuola					    *\n");
+	printf("*	07. Vai nella mail delle panzette				    *\n");
+	printf("*	08. Vai nella cartella degli eseguibili				    *\n");
+	printf("*	09. Vai nella cartella dei codici sorgente			    *\n");
+	printf("*	10. Crea/Modifica un file sorgente per un programma Java	    *\n");
+	printf("*	11. Compila programma Java					    *\n");
+	printf("*	12. Vai nella cartela di java					    *\n");
+	printf("*									    *\n");
+	printf("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n\n");
+	printf("	Che comando vuoi eseguire?: ");
 	scanf("%d", &scelta);
 	
 	switch(scelta)
@@ -53,6 +66,26 @@ int main(void)
 			gmailpanzette(scelta);
 		break;
 		
+		case 8:
+			vaiaeseguibili(scelta);
+		break;
+		
+		case 9:
+			vaiacodici(scelta);
+		break;
+		
+		case 10:
+			creaprogrammajava(scelta);
+		break;
+		
+		case 11:
+			compilajava(scelta);
+		break;
+		
+		case 12:
+			vaiajava(scelta);
+		break;
+		
 		default:
 			printf("\nOpzione non disponibile\n\n");
 		break;
@@ -72,7 +105,13 @@ int createsto(int a)
 		exit(1);
 	}
 	
-	fprintf(fd, "#!/bin/bash\ncd ../Desktop/Testo\ncat > file-nuovo.txt", x);
+	printf("\nInserisci il nome del file: ");
+	fflush(stdin);
+	scanf("%s", x);
+	
+	printf("Scrivi:\n");
+	
+	fprintf(fd, "#!/bin/bash\ncd ../Desktop/Testo\ncat > %s.txt", x);
 	
 	fclose(fd);
 	
@@ -90,7 +129,11 @@ int creaprogramma(int a)
 		exit(1);
 	}
 	
-	fprintf(fd, "#!/bin/bash\ncd ../Desktop/programmi/codici_sorgente\nsudo gedit nuovo-file.c", x);
+	printf("\nInserisci il nome del file: ");
+	fflush(stdin);
+	scanf("%s", x);
+	
+	fprintf(fd, "#!/bin/bash\ncd ../Desktop/programmi/codici_sorgente\nsudo gedit %s.c", x);
 	
 	fclose(fd);
 	
@@ -108,7 +151,11 @@ int compila(int a)
 		exit(1);
 	}
 	
-	fprintf(fd, "#!/bin/bash\ncd ../Desktop/programmi/codici_sorgente\ngcc nuovo-file.c -o ../eseguibili/cambia-nome", x);
+	printf("\nInserisci il nome del file: ");
+	fflush(stdin);
+	scanf("%s", x);
+	
+	fprintf(fd, "#!/bin/bash\ncd ../Desktop/programmi/codici_sorgente\ngcc %s.c -o ../eseguibili/%s", x , x);
 	
 	fclose(fd);
 	
@@ -188,5 +235,101 @@ int gmailpanzette(int a)
 	return 0;
 }
 
+int vaiaeseguibili(int a)
+{
+	FILE *fd;
+	char x[1000];
+	 
+	fd=fopen("scelta.sh", "w");
+	if (fd==NULL){
+		perror("Errore in apertura del file");
+		exit(1);
+	}
+	
+	fprintf(fd, "#!/bin/bash\ncd ../Desktop/programmi/eseguibili\nls\nsleep 5", x);
+	
+	fclose(fd);
+	
+	return 0;
+}
 
+int vaiacodici(int a)
+{
+	FILE *fd;
+	char x[1000];
+	 
+	fd=fopen("scelta.sh", "w");
+	if (fd==NULL){
+		perror("Errore in apertura del file");
+		exit(1);
+	}
+
+	fprintf(fd, "#!/bin/bash\ncd ../Desktop/programmi/codici_sorgente\nls\nsleep 5", x);
+	
+	fclose(fd);
+	
+	return 0;
+}
+
+int creaprogrammajava(int a)
+{
+	FILE *fd;
+	char x[1000];
+	 
+	fd=fopen("scelta.sh", "w");
+	if (fd==NULL){
+		perror("Errore in apertura del file");
+		exit(1);
+	}
+	
+	printf("\nInserisci il nome del file: ");
+	fflush(stdin);
+	scanf("%s", x);
+	
+	fprintf(fd, "#!/bin/bash\ncd ../Desktop/programmi/java\nsudo gedit %s.java", x);
+	
+	fclose(fd);
+	
+	return 0;
+}
+
+int compilajava(int a)
+{
+	FILE *fd;
+	char x[1000];
+	 
+	fd=fopen("scelta.sh", "w");
+	if (fd==NULL){
+		perror("Errore in apertura del file");
+		exit(1);
+	}
+	
+	printf("\nInserisci il nome del file: ");
+	fflush(stdin);
+	scanf("%s", x);
+	
+	fprintf(fd, "#!/bin/bash\ncd ../Desktop/programmi/java\njavac %s.java", x , x);
+	
+	fclose(fd);
+	
+	return 0;
+}
+
+int vaiajava(int a)
+{
+	FILE *fd;
+	char x[1000];
+	 
+	fd=fopen("scelta.sh", "w");
+	if (fd==NULL){
+		perror("Errore in apertura del file");
+		exit(1);
+	}
+
+	fprintf(fd, "#!/bin/bash\ncd ../Desktop/programmi/java\nls\nsleep 5", x);
+	
+	fclose(fd);
+	
+	return 0;
+}
 
